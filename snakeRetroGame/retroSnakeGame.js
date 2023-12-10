0aaf04eb061c78d81d633180a3ef2d896994283a
Casey Latimere Kreicar <kreicarcl@alumni.vcu.edu>
2023-12-09 19:06:12 -0500

    js next

--- snakeRetroGame/retroSnakeGame.js
+++ snakeRetroGame/retroSnakeGame.js
@@ -1,2 +1,6 @@
 // Retro Snake Game (a modified freeCodeCamp tutorial) //
 console.log('test');
+//Define HTML elements (variable board = objectNameDocument.methodNameGetElementByID('html element id'))
+const board = document.getElementById('game-board');
+//test 
+console.log(board);

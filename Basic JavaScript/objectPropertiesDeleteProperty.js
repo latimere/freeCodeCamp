a21f22ff4206d88d4ca93174c45bd25e78613206
Casey Latimere Kreicar <kreicarcl@alumni.vcu.edu>
2024-01-09 13:04:37 -0500

    3 solutions

---
+++ Basic JavaScript/objectPropertiesDeleteProperty.js
@@ -0,0 +1,11 @@
+//Object
+const myDog = {
+  //Property Name : Property Value
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+//Delete Property
+delete myDog.tails;
---
+++ Basic JavaScript/objectPropertyAddNewProperty.js
@@ -0,0 +1,11 @@
+//Object
+const myDog = {
+  //Property Name : Property Value
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+//Add Property name and property value using dot notation to myDog object
+myDog.bark = "Woof Woof"

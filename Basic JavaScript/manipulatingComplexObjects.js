188e7a8e4c566a8500e99ab2f090eceb8cce9cda
Casey Latimere Kreicar <kreicarcl@alumni.vcu.edu>
2025-08-25 06:09:38 -0400

    started working on JS certificate again

---
+++ Basic JavaScript/manipulatingComplexObjects.js
@@ -0,0 +1,17 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": ["CD", "8T", "LP"],
+    "gold": true
+  },
+  {
+    "artist": "Daft Punk",
+    "title": "One more time",
+    "release_year": 2001,
+    "formats": ["CD", "Cassette"],
+    "gold": true
+  },
+];
+console.log(myMusic)
---
+++ Basic JavaScript/whileLoops.js
@@ -0,0 +1,10 @@
+// Setup
+const myArray = [];
+
+// Only change code below this line
+var y = 5;
+while(y >=0 ) {
+  myArray.push(y);
+  y--;
+}
+console.log(myArray)
